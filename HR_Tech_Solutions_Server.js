fbfd2e69707f06d4e52728e0ae6f92b43d6abf27
ViVek6301 <62293550+ViVek6301@users.noreply.github.com>
2021-09-13 11:38:20 +0530

    Added Logger

--- HR_Tech_Solutions_Server.js
+++ HR_Tech_Solutions_Server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const EmployeeServices = require('./Routes/Employee_Services');
 const AdminServices = require("./Routes/Admin_Services");
 const connection = require("./models/db");
+const morgan = require("morgan");
 
 var app = express();
 app.listen(7091);
@@ -10,6 +11,7 @@ app.listen(7091);
 console.log("Employee Management Server is Running...");
 
 app.use(bodyParser.json());
+app.use(morgan('combined'));
 
 app.use('/api/employee_services', EmployeeServices);
 app.use('/api/admin_services', AdminServices);
