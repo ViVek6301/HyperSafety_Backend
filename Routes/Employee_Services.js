62b9edde98439a0c408f06dd13bad759183f671b
ViVek6301 <62293550+ViVek6301@users.noreply.github.com>
2021-09-14 12:38:40 +0530

    Added image upload to imgur

--- Routes/Employee_Services.js
+++ Routes/Employee_Services.js
@@ -3,32 +3,66 @@ const ImageUpload = require("../Utilities/ImageUpload");
 const fs = require("fs");
 const path = require("path");
 const sqlConnection = require("../models/db");
+const request = require("request");
 const auth = require("../Utilities/auth");
+require('dotenv').config();
 
 const upload = ImageUpload.upload;
 router = express.Router();
 
 router
     // Adding New Employee with Image.
-    .post("/", auth.verifyToken, upload.single('employee_image'), (req, res, next) => { // expecting image(employee_image), empId and empName as fields
-        const checkEmployeeQuery = "SELECT 1 FROM Employees WHERE EmployeeId = \'" + req.body.empId + "\';";
+    .post("/", auth.verifyToken, upload.single('employee_image'), (req, res, next) => { // expecting image(employee_image), empID and empName as fields
+        const checkEmployeeQuery = "SELECT 1 FROM Employees WHERE EmployeeId = \'" + req.body.empID + "\';";
         sqlConnection.query(checkEmployeeQuery, (error, results, fields) => {
             if (error) {
                 next(new Error("Error - Try Again."));
                 return;
             }
             if (results.length == 0) {
-                const createEmployeeQuery = "INSERT INTO Employees(EmployeeName, EmployeeId) " +
-                    "VALUES (\'" + req.body.empName + "\',\'" + req.body.empId + "\');";
-                sqlConnection.query(createEmployeeQuery, (error, results, fields) => {
-                    if (error) {
-                        throw error;
+                const fileName = 'temp' + path.extname(req.file.originalname);
+                fs.renameSync(req.file.path, req.file.path.replace('temp', fileName));
+                var imgurUploadRequest = {
+                    'method': 'POST',
+                    'url': 'https://api.imgur.com/3/image',
+                    'headers': {
+                        'Authorization': 'Bearer ' + process.env.IMGUR_ACCESS_TOKEN
+                    },
+                    formData: {
+                        "image": fs.createReadStream("./images/" + fileName),
+                        "album": process.env.IMGUR_ALBUM_ID,
+                        "type": "file"
                     }
-                    // console.log(results);
-                    fs.renameSync(req.file.path, req.file.path.replace('temp', req.body.empName
-                        + "_" + req.body.empId + path.extname(req.file.originalname)));
-                    res.end("Employee Added Successfully");
+                    // 'maxRedirects': 20
+                };
+
+                request(imgurUploadRequest, function (error, response) {
+                    if (error) {
+                        console.log(error);
+                        res.json(error);
+                    }
+                    var imgurUploadResponse = JSON.parse(response.body);
+                    if (imgurUploadResponse.data.error) {
+                        console.log(imgurUploadResponse.data.error);
+                        res.status(imgurUploadResponse.status).send(imgurUploadResponse.data.error);
+                        return;
+                    }
+                    fs.unlink("./images/" + fileName, (err) => {
+                        if (err) {
+                            throw new Error("Error - Try Again.");
+                        }
+                    });
+                    const createEmployeeQuery = "INSERT INTO Employees(EmployeeName, EmployeeId, ImageID, ImageURL) " +
+                        "VALUES (\'" + req.body.empName + "\',\'" + req.body.empID + "\',\'" + imgurUploadResponse.data.id
+                        + "\',\'" + imgurUploadResponse.data.link + "\');";
+                    sqlConnection.query(createEmployeeQuery, (error, results, fields) => {
+                        if (error) {
+                            throw error;
+                        }
+                        res.end("Employee Added Successfully");
+                    });
                 });
+
             }
             else {
                 next(new Error("Employee Already Exists.")); // forcefully send to error-handling middleware
@@ -37,45 +71,60 @@ router
     })
 
     // Deleting Employee by deleting their Image.
-    .delete("/", auth.verifyToken, (req, res, next) => { // expecting empId and empName as fieldss
-
-        var fileName = req.body.empName + "_" + req.body.empId;
-        var imageFound = true;
-        if (fs.existsSync("./images/" + fileName + ".jpg")) {
-            fileName += ".jpg";
-        }
-        else if (fs.existsSync("./images/" + fileName + ".png")) {
-            fileName += ".png";
-        }
-        else {
-            imageFound = false;
-        }
-        const deleteEmployeeQuery = "DELETE FROM Employees WHERE EmployeeId=\'" + req.body.empId
-            + "\' AND EmployeeName=\'" + req.body.empName + "\'";
-        sqlConnection.query(deleteEmployeeQuery, (error, results, fields) => {
+    .delete("/", auth.verifyToken, (req, res, next) => {
+        const findImageIDQuery = "SELECT ImageID FROM Employees WHERE EmployeeID =\'" + req.body.empID
+            + "\' AND EmployeeName=\'" + req.body.empName + "\';";
+        sqlConnection.query(findImageIDQuery, (error, results, fields) => {
             if (error) {
                 next(new Error("Error - Try Again."));
                 return;
             }
-            var affectedRows = results.affectedRows;
-            if (affectedRows === 0) {
+            if (results.length == 0) {
                 next(new Error("Employee Not Found."));
+                return;
             }
             else {
-                if (imageFound) {
-                    fs.unlink("./images/" + fileName, (err) => {
-                        if (err) {
-                            throw new Error("Error - Try Again.");
+                const imageID = results[0].ImageID;
+                const deleteEmployeeQuery = "DELETE FROM Employees WHERE EmployeeId=\'" + req.body.empID
+                                            + "\' AND EmployeeName=\'" + req.body.empName + "\'";
+                sqlConnection.query(deleteEmployeeQuery, (error, results, fields) => {
+                    if (error) {
+                        next(new Error("Error - Try Again."));
+                        return;
+                    }
+
+                    var imgurDeleteRequest = {
+                        'method': 'DELETE',
+                        'url': 'https://api.imgur.com/3/image/' + imageID,
+                        'headers': {
+                            'Authorization': 'Bearer ' + process.env.IMGUR_ACCESS_TOKEN
                         }
+                        // 'maxRedirects': 20
+                    };
+                
+                    request(imgurDeleteRequest, function(error, response) {
+                        if (error) {
+                            console.log(error);
+                            res.setHeader("Content-Type", "application/json");
+                            res.status(500).json(error);
+                            return;
+                        }
+                        var imgurDeleteResponse = JSON.parse(response.body);
+                        if (imgurDeleteResponse.data.error) {
+                            console.log(imgurDeleteResponse.data.error);
+                            res.status(imgurDeleteResponse.status).send(imgurDeleteResponse.data.error);
+                            return;
+                        }
+                        res.send("Employee Successfully Deleted.");
                     });
-                }
-                res.end("Employee Successfully Deleted.");
+
+                });
             }
         });
     })
 
     .put("/", auth.verifyToken, (req, res, next) => {
-        const resetRecordQuery = "UPDATE Employees SET Warnings = 0 WHERE EmployeeID = \'" + req.body.empId
+        const resetRecordQuery = "UPDATE Employees SET Warnings = 0 WHERE EmployeeID = \'" + req.body.empID
             + "\' AND EmployeeName = \'" + req.body.empName + "\'";
         sqlConnection.query(resetRecordQuery, (error, results, fields) => {
             if (error) {
@@ -116,12 +165,10 @@ router
         });
     })
 
-    .get("/:empId/:empName", (req, res, next) => {
-        const getEmployeesQuery = "SELECT * FROM Employees WHERE binary `EmployeeID` = \""
-            + req.params.empId + "\" and binary `EmployeeName` = \"" + req.params.empName + "\"";
+    .get("/:empID/:empName", auth.verifyToken, (req, res, next) => {
+        const getEmployeesQuery = "SELECT * FROM Employees WHERE EmployeeID = \""
+            + req.params.empID + "\" and EmployeeName = \"" + req.params.empName + "\"";
         sqlConnection.query(getEmployeesQuery, (error, results, fields) => {
-            console.log(getEmployeesQuery);
-            console.log(results);
             if (error) {
                 next(new Error("Error - Try Again."));
                 return;
